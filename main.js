2e8f44bba938e93671f1ea8540987019092ad496
goldshoot0720 <goldshoot0720@gmail.com>
2025-03-13 23:32:27 +0800

    Electron

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Disable nodeIntegration for security purposes
+      nodeIntegration: false,
+      contextIsolation: true, // This is more secure
+      preload: path.join(__dirname, 'preload.js')  // Use preload.js if needed
+    },
+  });
+
+  // Load the specified URL (or use loadFile for local assets)
+  win.loadURL('https://feng37.fwh.is/Eroll3Feng37/');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
